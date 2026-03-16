0052f519366d0f5cd6f6d0d487cbc016adcb96d7
Jamie <jamie@Jamieui-Macmini.local>
2026-03-17 00:34:26 +0900

    fix(blog): CategoriesClient no longer passes tags (categories fixed list)

--- jamie-next-blog/src/components/CategoriesClient.tsx
+++ jamie-next-blog/src/components/CategoriesClient.tsx
@@ -3,5 +3,5 @@ import React from 'react'
 import Categories from './Categories'
 
 export default function CategoriesClient({tags}:{tags:string[]}){
-  return <Categories tags={tags} />
+  return <Categories />
 }
